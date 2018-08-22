85f49aca38a96f674fd5b6bba939c843b8d1cda9
cloudzq <33003023+cloudzq@users.noreply.github.com>
2018-08-22 14:07:44 +0800

    projectTasks
    projectTasks

---
+++ projectTasks.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+"""
+@Project:watchmen
+@Language:Python3.6.4
+@Author:Hans
+@File:projectTasks.py
+@Ide:PyCharm
+@Time:2018/8/22 11:39
+@Remark:
+"""
+
+import datetime
+
+def projectConfig():
+    pass
