19a2fa301a77645c38815cbc0155f6a49040c81b
cloudzq <33003023+cloudzq@users.noreply.github.com>
2018-08-22 14:02:01 +0800

    app
    app

---
+++ app.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+"""
+@Project:watchmen
+@Language:Python3.6.4
+@Author:Hans
+@File:app.py
+@Ide:PyCharm
+@Time:2018/8/22 10:27
+@Remark:
+"""
+
+import os
+from celery import Celery
+import django
+from .config import CeleryConfig
+
+
+__all__ = ['app']
+
+class Mycelery(Celery):
+    def now(self):
+        from datetime import datetime
+        return datetime.now(self.timezone)
+
+os.environ.setdefault('DJANFGO_SETTINGS_MODULE','watchmen.settings')
+django.setup()
+app = Mycelery('data_watchmen')
+app.config_from_object(CeleryConfig)
+app.autodiscover_tasks()
