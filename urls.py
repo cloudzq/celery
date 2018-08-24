3d0272f6a4f8840327d75947205ada41b5138452
cloudzq <33003023+cloudzq@users.noreply.github.com>
2018-08-24 10:38:22 +0800

    1
    1

--- urls.py
+++ urls.py
@@ -11,13 +11,15 @@
 
 from django.conf.urls import url
 
-from .import view
+from . import view
+
+app_label = 'project'
 
 urlpatterns = [
-    url(r'^project/add_task/$',view.add_task),#添加任务
-    url(r'^project/execute_task/$',view.execute_task_by_name),#异步任务执行
-    url(r'^project/query_id/$',view.query_task_by_id),#异步结果查询
-    url(r'^project/switch_task/$',view.switch_task_by_name),#启用禁用任务
-    url(r'^project/query_name/$',view.query_task_by_taskname),#查看已建立的任务
-    url(r'^project/delete_task/$',view.delete_task_by_taskname),#删除已建立的任务
+    url(r'^project/add_task/$', view.add_task),  # 添加任务
+    url(r'^project/execute_task/$', view.execute_task_by_name),  # 异步任务执行
+    url(r'^project/query_id/$', view.query_task_by_id),  # 异步结果查询
+    url(r'^project/switch_task/$', view.switch_task_by_name),  # 启用禁用任务
+    url(r'^project/query_name/$', view.query_task_by_taskname),  # 查看已建立的任务
+    url(r'^project/delete_task/$', view.delete_task_by_taskname),  # 删除已建立的任务
 ]
